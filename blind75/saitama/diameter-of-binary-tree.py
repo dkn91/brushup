d435bb317b5bf1484b4bbc2c608e74e2d364aa03
Deepak Pishe <deepakpishe@outlook.com>
2025-09-25 23:20:43 -0700

    adding TreeNode class

--- blind75/saitama/diameter-of-binary-tree.py
+++ blind75/saitama/diameter-of-binary-tree.py
@@ -1,3 +1,10 @@
+# LeetCode's TreeNode definition
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val        # node value
+        self.left = left      # left child (TreeNode or None)
+        self.right = right    # right child (TreeNode or None)
+
 class Solution:
     def diameterOfBinaryTree(self, root):
         self.diameter = 0
